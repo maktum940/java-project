d580abd8101d288a50b2b194ecad7b3698492be3
maktum940 <64151360+maktum940@users.noreply.github.com>
2024-10-14 20:43:55 +0600

    Delete MidtermExam directory

--- MidtermExam/src/products/Products.java
+++
@@ -1,8 +0,0 @@
-package products;
-
-public class Products extends ProductInformation{
-
-    public Products(String productName, String productCategory, String productSubCategory, String expireDate, String purchaseDate, double productPrice) {
-        super(productName, productCategory, productSubCategory, expireDate, purchaseDate, productPrice);
-    }
-}
